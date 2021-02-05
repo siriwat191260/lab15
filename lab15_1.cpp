38dcbc81213d444cc6f8827480e48aec963c4240
siriwat191260 <pan-191260@hotmail.com>
2021-02-05 16:09:39 +0700

    finish

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -32,3 +32,46 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *a,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(a+i);
+        if((i+1)%M==0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *a,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        *(a+i) = (rand()%101)/100.;
+    }
+}
+
+void findRowSum(const double *a,double *b,int N,int M){
+    double sum;
+    int z=0;
+    for(int i = 0; i < (N*M); i++){
+        sum+=*(a+i);
+        if((i+1)%M==0) {
+        *(b+z)=sum;
+        sum=0;
+        z+=1;
+        }
+    }
+}
+
+void findColSum(const double *a,double *b,int N,int M){
+    double sum;
+    for(int i = 0; i<M; i++){
+    if(N==1) *(b+i)=*(a+i);
+    else{
+            sum=*(a+i);
+            for(int z=i+M;z<(N*M);z+=M){
+            sum+=*(a+z);
+            }
+            *(b+i)=sum;
+        }
+    }
+    
+}
